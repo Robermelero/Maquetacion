c076adf8c92b7accff89a4bdb95fa2847a2a1af3
Roberto Melero <roberto.meleros@gmail.com>
2023-05-16 11:27:41 +0200

    22

--- Maquetacion basica/viajes.js
+++ Maquetacion basica/viajes.js
@@ -23,36 +23,22 @@ bbdd.push(searching)
 console.log(bbdd)
 } 
 ////////////////// FUNCTION FILTER ////////////
-let bbdd2 = []
-let filtrado = []
+
+
+
 const filtro = () =>{
-    let ids =  document.getElementById("nombres").value.toLowerCase();
-    let ori = document.getElementById("origen").value.toLowerCase();
-    let dest = document.getElementById("destino").value.toLowerCase();
-    let pers = document.getElementById("personas").value.toLowerCase();
-    let fech = document.getElementById("fecha").value.toLowerCase();
-    
-    if(ids == "" || ori == "" || dest == "" || pers == "" || fech == ""){
-        alert("Introducir todos los campos").focus(); }   
-    
-        let searching ={
-        name: ids,
-        origen: ori,
-        destino: dest,
-        nºPersonas: pers,
-        fechaSalida: fech,
-    }
-bbdd2.push(searching)
-let miau = bbdd2.filter(filt => ( searching.destino =="mallorca")||(searching.destino =="galicia")||(searching.destino=="canarias"));
-filtrado.push(miau)
-// console.log(filtrado)
-let miau2 = []
-for( let i = 0; i<filtrado.length; i++){
 
-    miau2[i]= filtrado[i]
+let mostrar = '';
+let miau = bbdd.filter(filt => ( filt.destino =="mallorca")||(filt.destino =="galicia")||(filt.destino=="canarias"));
+console.log('**************************');
+console.log(miau);
+
+
+for( let i = 0; i<miau.length; i++){
+    mostrar += `${miau[i].name}  ${miau[i].destino}`
 }
+console.log('**************************');
+console.log(mostrar);
 
-document.getElementById("ofert").innerHTML = miau2
+document.getElementById('ofert').innerHTML = mostrar
 }
-
-
